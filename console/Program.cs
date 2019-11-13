f6d4e054a96b744ae0b4ef384b806c844e244ef9
Veli Bacik <velibacik60@gmail.com>
2019-11-13 15:01:36 +0300

    user init completed

--- console/Program.cs
+++ console/Program.cs
@@ -19,14 +19,7 @@ namespace console
 		private static IDeviceService deviceService;
 		async static Task Main(string[] args)
 		{
-			user = new User();
-			user.MQTTuserName = "sem5VK";
-			user.MQTTpassword = "EOUH2#bW";
-			user.CSEid = "/sem-cux_bac8066f-dfd1-49cd-bee4-147e5e0dd267";
-			user.clientid = "CAE590914ba-2358-4634-b500-58232fbc4d39";
-			user.MQTTport = 1445;
-			user.timeout = 30;
-			user.MQTTpointOfAcsesss = "mqtt.kocdigital.com";
+			user = User.initUser();
 
 			await platform360InitAsync("veli");
 			startTimer();
